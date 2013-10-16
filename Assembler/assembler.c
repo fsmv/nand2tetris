7b164a6205c194d7167fc694fbf92c11f754c64f
Andrew Kallmeyer <kallmeyeras@gmail.com>
2013-10-16 13:35:20 -0400

    Refactored to make main.c an interface to the actual assembler which is now in assembler.c

---
+++ Assembler/assembler.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "assembler.h"
+#include "parser.h"
+
+size_t putBytes(unsigned short bytes, unsigned char** arr, size_t index, size_t size) {
+    int result = size;
+
+    if(index + 2 > size) {
+        char* temp = malloc(2*size);
+        memcpy(temp, *arr, size);
+        free(*arr);
+        *arr = temp;
+
+        result = 2*size;
+    }
+    
+    (*arr)[index] = (unsigned char) (bytes >> 8);
+    (*arr)[index+1] = (unsigned char) (bytes & 0xff);
+
+    return result;
+}
+
+void shrink(unsigned char** arr, size_t newlen) {
+    char* temp = malloc(newlen);
+    memcpy(temp, *arr, newlen);
+    free(*arr);
+    *arr = temp;
+}
+
+void output(FILE* f, unsigned char* data, size_t datac) {
+    int i;
+    for(i = 0; i < datac/2; i++) {
+        unsigned short code = (data[2*i] << 8) + (data[2*i+1]);
+        char temp[18];
+        temp[16] = '\n';
+        temp[17] = '\0';
+        
+        int j;
+        for(j = 0; j < 16; j++) {
+            temp[j] = (code >> 15-j) & 1 ? '1' : '0';
+        }
+
+        fprintf(f, "%s", temp);
+    }
+}
+
+size_t assemble(FILE* f, unsigned char* out[]) {
+    char buff[256];
+    int i,n = 0;
+    int c;
+
+    //start with room for 32 instructions
+    unsigned char* result = malloc(64);
+    size_t rsize = 64;
+
+    do {
+        c = fgetc(f);
+        
+        if(c != '\n'){
+            if(i >= 256){
+                fprintf(stderr, "Line %d too long (max 256), buffer overflow\n", n);
+                abort();
+            }
+
+            buff[i] = c;
+            i++;
+        }else{
+            buff[i] = '\0';
+            i = 0;
+
+            instruction* op;
+            op = parseLine(buff);
+
+            if(op != NULL) {
+                unsigned short data;
+                if(op->type == A) {
+                    data = op->literal;
+                    data = data & 0x7fff;
+                }else{
+                    data = (0x7 << 13) + (op->comp << 6) + (op->dest << 3) + (op->jump);
+                }
+
+                rsize = putBytes(data, &result, 2*n, rsize);
+                free(op);
+
+                n++;
+            }
+        }
+    }while(c != EOF);
+
+    shrink(&result, 2*n);
+    //output(stdout, result, 2*n);
+    *out = result;
+    return 2*n;
+}
---
+++ Assembler/assembler.h
@@ -0,0 +1,37 @@
+#ifndef ASSEMBLER_H
+#define ASSEMBLER_H
+
+/**
+ * puts the bytes in the array at the specified index and resizes if necessary
+ * returns the size of the array whether it was changed or not
+ */
+size_t putBytes(unsigned short bytes, unsigned char** arr, size_t index, size_t size);
+
+/**
+ * resizes the array to be newlen bytes long and copies newlen bytes from the
+ * old array to the new one
+ */
+void shrink(unsigned char** arr, size_t newlen);
+
+/**
+ * prints the data in the array to f as text printing two bytes of binary per
+ * line with the most significant bit on the left
+ *
+ * ex:
+ * data = {0xff, 0xff, 0xAA, 0xAA}
+ * output:
+ * 1111111111111111
+ * 1010101010101010
+ */
+void output(FILE* f, unsigned char* data, size_t datac);
+
+/**
+ * assembles the file f by putting the bytecode in the out array and returning
+ * the size of the out array.
+ *
+ * the pointer will be overwritten to an array stored on the heap. Make sure to
+ * free it later.
+ */
+size_t assemble(FILE* f, unsigned char* out[]);
+
+#endif
